0dd3a187831e6663f309d8c9c1dca96e5e330a5f
tradixcodes <tradixiv@gmail.com>
2026-01-09 17:23:13 +0300

    C: Game logic working

--- main.lua
+++ main.lua
@@ -6,27 +6,61 @@ function love.load()
 
 	score = 0
 	timer = 0
+	-- 1- main menu
+	-- 2- game is playing
+	gameState = 1
 
 	gameFont = love.graphics.newFont(40)
+
+	sprites = {}
+	sprites.sky = love.graphics.newImage("sprites/sky.png")
+	sprites.crosshairs = love.graphics.newImage("sprites/crosshairs.png")
+	sprites.target = love.graphics.newImage("sprites/target.png")
+
+	love.mouse.setVisible(false)
 end
 
-function love.update(dt) end
+function love.update(dt)
+	if timer > 0 then
+		timer = timer - dt
+	end
+
+	if timer < 0 then
+		timer = 0
+		gameState = 1
+	end
+end
 
 function love.draw()
-	love.graphics.setColor(1, 0, 0)
-	love.graphics.circle("fill", target.x, target.y, target.radius)
+	love.graphics.draw(sprites.sky, 0, 0)
 
 	love.graphics.setColor(1, 1, 1)
 	love.graphics.setFont(gameFont)
-	love.graphics.print(score, 0, 0)
+	love.graphics.print("Score: " .. score, 5, 5)
+	love.graphics.print("Time: " .. math.ceil(timer), love.graphics.getWidth() - 300, 0)
+
+	if gameState == 1 then
+		love.graphics.printf("Click anywhere to begin!", 0, 250, love.graphics.getWidth(), "center")
+	end
+
+	if gameState == 2 then
+		love.graphics.draw(sprites.target, target.x - target.radius, target.y - target.radius)
+	end
+	love.graphics.draw(sprites.crosshairs, love.mouse.getX() - 20, love.mouse.getY() - 20)
 end
 
 function love.mousepressed(x, y, button, istouch, presses)
-	if button == 1 then
+	if button == 1 and gameState == 2 then
 		local mouseToTarget = distanceBetween(x, y, target.x, target.y)
 		if mouseToTarget < target.radius then
 			score = score + 1
+			target.x = math.random(target.radius, love.graphics.getWidth() - target.radius)
+			target.y = math.random(target.radius, love.graphics.getHeight() - target.radius)
 		end
+	elseif button == 1 and gameState == 1 then
+		gameState = 2
+		timer = 10
+		score = 0
 	end
 end
 
